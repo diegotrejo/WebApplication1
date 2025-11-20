6960579a2c0caa9a924b6d496212dc12ad0b2f93
Diego Trejo <diego_trejo@hotmail.com>
2025-11-19 21:37:42 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new List<User> {
+                new User { Id = 1, Name = "Alice", Email = "jjjj" },
+                new User { Id = 2, Name = "Bob", Email = "bbbb" },
+                new User { Id = 3, Name = "Charlie", Email = "cccc" }
+            };
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return new User { Id = 1, Name = "Alice", Email = "jjjj" };
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public User Post([FromBody] User user)
+        {
+            user.Id = 44; // Simulate assigning a new ID
+            return user;
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public User Put(int id, [FromBody] User user)
+        {
+            return user;
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+            builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
